3cd1f94e1ddb5b154f63994ba2d71cc9586da4fd
Tabbytech <tabbyccherop@gmail.com>
2025-09-07 21:47:26 +0300

    Update models.py

--- listings/models.py
+++ listings/models.py
@@ -14,7 +14,8 @@ class Listing(models.Model):
     pricepernight = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    class Meta:
+        app_label = 'listings'
     def __str__(self):
         return self.name
 
