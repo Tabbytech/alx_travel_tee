2a2c708e6d89c6da00d3fbbc63a57d0445eac4a8
Tabbytech <tabbyccherop@gmail.com>
2025-09-07 19:41:17 +0300

    Delete alx_travel_app_0x03/listings directory

--- alx_travel_app_0x03/listings/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import ListingViewsets, BookingViewsets
-
-router = DefaultRouter()
-router.register(r'listings', ListingViewsets, basename='listing')
-router.register(r'bookings', BookingViewsets, basename='booking')
-
-urlpatterns = router.urls
